991ee795fe4903b231ad01da09c0556ed8984ea0
Thibault Lanternier <thibault@tocosk.com>
2017-04-24 14:51:13 +0200

    Création automatique d'une nouvelle révision de Task à partir d'une image Docker

--- updateTask.js
+++ updateTask.js
@@ -6,27 +6,24 @@ var ecsClient = new AWS.ECS({
     region: "eu-west-1",
     apiVersion: "2014-11-13"
 });
-console.log(minimist(process.argv.slice(2)));
-//var argv = parseArgs(args,{});
-//ecsClient.registerTaskDefinition({})
+var requestParams = minimist(process.argv.slice(2));
 ecsClient.describeTaskDefinition({
-    taskDefinition: "PDFGenerator"
+    taskDefinition: requestParams.taskName
 }, function (err, data) {
     if (err != null) {
         console.error(err);
     }
     else {
-        var UpdateRequest = taskDefinitionHelper_1.taskDefinitionHelper.GetTaskDefinitionUpdateRequest(data, {
-            "pdfgenerator": "lfde/pdfgenerator:jenkins_8"
-        });
+        var containerImageValues = {};
+        containerImageValues[requestParams.containerName] = requestParams.dockerImage;
+        var UpdateRequest = taskDefinitionHelper_1.taskDefinitionHelper.GetTaskDefinitionUpdateRequest(data, containerImageValues);
         ecsClient.registerTaskDefinition(UpdateRequest, function (err, data) {
             if (err != null) {
                 console.error(err);
             }
             else {
-                console.log(data);
+                console.log(`${data.taskDefinition.family}:${data.taskDefinition.revision}`);
             }
         });
     }
 });
-//ecsClient.registerTaskDefinition({}) 
--- updateTask.ts
+++ updateTask.ts
@@ -7,35 +7,35 @@ var ecsClient = new AWS.ECS({
     apiVersion : "2014-11-13"
 });
 
-interface inputParam{
+interface updateTaskParam{
     _ : string[];
     taskName : string;
     containerName : string;
     dockerImage : string;
 }
 
-console.log(minimist(process.argv.slice(2)));
-//var argv = parseArgs(args,{});
-//ecsClient.registerTaskDefinition({})
+var requestParams : updateTaskParam = <updateTaskParam>minimist(process.argv.slice(2));
 
 ecsClient.describeTaskDefinition({
-    taskDefinition : "PDFGenerator"
+    taskDefinition : requestParams.taskName
 },function(err,data){
     if(err != null){
         console.error(err);
     }else{
-        var UpdateRequest = taskDefinitionHelper.GetTaskDefinitionUpdateRequest(data,{
-            "pdfgenerator" : "lfde/pdfgenerator:jenkins_8"
-        });
+        var containerImageValues = {};
+        containerImageValues[requestParams.containerName] = requestParams.dockerImage;
+
+        var UpdateRequest = taskDefinitionHelper.GetTaskDefinitionUpdateRequest(
+            data,
+            containerImageValues
+        );
 
         ecsClient.registerTaskDefinition(UpdateRequest,function(err,data){
             if(err != null){
                 console.error(err);
             }else{
-                console.log(data);
+                console.log(`${data.taskDefinition.family}:${data.taskDefinition.revision}`);
             }
         });
     }
 });
-
-//ecsClient.registerTaskDefinition({})
